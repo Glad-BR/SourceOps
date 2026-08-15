5dd5599fbdcbf69d1a674940d174a1d3db06a02e
Glad-BR <gradbeerro@proton.me>
2026-08-15 16:10:12 -0300

    Clanup metarials types.py

--- addon/types/material_export/types.py
+++ addon/types/material_export/types.py
@@ -1,17 +1,13 @@
 import numpy as np
 
 from dataclasses import dataclass
-from enum import Enum
 from pathlib import Path
 from sourcepp import vtfpp
-Flags = vtfpp.VTF.Flags
-ImageFormat = vtfpp.ImageFormat
 
 from ...props.material_props import SOURCEOPS_AllMaterialsProps
 
 @dataclass
 class ExportTexture:
-    #image: Image.Image
     image: np.ndarray
     image_hash: str
     image_name: str
@@ -19,8 +15,8 @@ class ExportTexture:
 
     output_path: Path = None
 
-    format: ImageFormat = None
-    flags: tuple[Flags, ...] = ()
+    format: vtfpp.ImageFormat = None
+    flags: tuple[vtfpp.VTF.Flags, ...] = ()
     invert_green: bool = False
 
 @dataclass
@@ -31,9 +27,3 @@ class ExportMaterial:
     emissive: ExportTexture = None
     phong: ExportTexture = None
     envmapmask: ExportTexture = None
-
-class PIL_VTF_map(Enum):
-    RGB = ImageFormat.RGB888
-    RGBA = ImageFormat.RGBA8888
-    L = ImageFormat.I8
-    LA = ImageFormat.IA88
